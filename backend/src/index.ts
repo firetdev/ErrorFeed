0837b4a45107be7fbf81fe3d8a1fdee1e8f09d9d
firetdev <futuregamesstore@gmail.com>
2026-08-20 23:49:53 -0400

    Undo comment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,7 @@ import logsRouter from './routes/logs.js';
 import { initializeWebSocketServer } from './websocket/server.js';
 
 const app = express();
-// moving in to college
+
 app.use(cors({
   origin: 'http://localhost:5173'
 }));
